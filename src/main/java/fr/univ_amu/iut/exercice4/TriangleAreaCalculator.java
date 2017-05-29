94443733cc70c6c011ca2e5930fcd85c5fdf05f0
Sébastien NEDJAR <sebastien@nedjar.com>
2017-05-29 19:35:18 +0200

    Squelette de l'exo4

--- src/main/java/fr/univ_amu/iut/exercice4/TriangleAreaCalculator.java
+++ src/main/java/fr/univ_amu/iut/exercice4/TriangleAreaCalculator.java
@@ -2,16 +2,11 @@ package fr.univ_amu.iut.exercice4;
 
 import fr.univ_amu.iut.exercice3.TriangleArea;
 import javafx.application.Application;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 
@@ -40,17 +35,7 @@ public class TriangleAreaCalculator extends Application {
     private GridPane root = new GridPane();
 
     private static void configSlider(Slider slider) {
-        slider.setMin(0);
-        slider.setMax(10);
-        slider.setValue(0);
-
-        slider.setSnapToTicks(true);
-        slider.setShowTickLabels(true);
-        slider.setShowTickMarks(true);
-
-        slider.setMajorTickUnit(5);
-        slider.setMinorTickCount(5);
-        slider.setBlockIncrement(1);
+        throw new RuntimeException("Not yet implemented !");
     }
 
     @Override
@@ -71,73 +56,27 @@ public class TriangleAreaCalculator extends Application {
     }
 
     private void configSliders() {
-        configSlider(x1Slider);
-        configSlider(y1Slider);
-        configSlider(x2Slider);
-        configSlider(y2Slider);
-        configSlider(x3Slider);
-        configSlider(y3Slider);
+        throw new RuntimeException("Not yet implemented !");
     }
 
     private void configGridPane() {
-        root.setPadding(new Insets(10, 10, 10, 10));
-        root.setHgap(10);
-        root.setVgap(20);
-
-        ColumnConstraints column1 = new ColumnConstraints(50, 50, Double.MAX_VALUE);
-        ColumnConstraints column2 = new ColumnConstraints(150, 450, Double.MAX_VALUE);
-        column2.setHgrow(Priority.ALWAYS);
-        root.getColumnConstraints().addAll(column1, column2);
+        throw new RuntimeException("Not yet implemented !");
 
     }
 
     private void addArea() {
-        root.add(areaLabel, 0, 10);
-        root.add(areaTextField, 1, 10);
-        GridPane.setVgrow(areaLabel, Priority.ALWAYS);
+        throw new RuntimeException("Not yet implemented !");
     }
 
     private void addSliders() {
-        root.add(x1Label, 0, 1);
-        root.add(x1Slider, 1, 1);
-
-        root.add(y1Label, 0, 2);
-        root.add(y1Slider, 1, 2);
-
-        root.add(x2Label, 0, 4);
-        root.add(x2Slider, 1, 4);
-
-        root.add(y2Label, 0, 5);
-        root.add(y2Slider, 1, 5);
-
-        root.add(x3Label, 0, 7);
-        root.add(x3Slider, 1, 7);
-
-        root.add(y3Label, 0, 8);
-        root.add(y3Slider, 1, 8);
+        throw new RuntimeException("Not yet implemented !");
     }
 
     private void addPointLabels() {
-        Label p1 = new Label("P1");
-        GridPane.setConstraints(p1, 0, 0, 2, 1, HPos.CENTER, VPos.BASELINE);
-
-        Label p2 = new Label("P2");
-        GridPane.setConstraints(p2, 0, 3, 2, 1, HPos.CENTER, VPos.BASELINE);
-
-        Label p3 = new Label("P3");
-        GridPane.setConstraints(p3, 0, 6, 2, 1, HPos.CENTER, VPos.BASELINE);
-        root.getChildren().addAll(p1, p2, p3);
+        throw new RuntimeException("Not yet implemented !");
     }
 
     private void createBinding() {
-        triangleArea.x1Property().bind(x1Slider.valueProperty());
-        triangleArea.x2Property().bind(x2Slider.valueProperty());
-        triangleArea.x3Property().bind(x3Slider.valueProperty());
-
-        triangleArea.y1Property().bind(y1Slider.valueProperty());
-        triangleArea.y2Property().bind(y2Slider.valueProperty());
-        triangleArea.y3Property().bind(y3Slider.valueProperty());
-
-        areaTextField.textProperty().bind(triangleArea.areaProperty().asString());
+        throw new RuntimeException("Not yet implemented !");
     }
 }
