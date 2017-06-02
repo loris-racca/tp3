8de673c96e144d35dea08a8da5b0ca8376cf052b
RACCA Loris <r16003406@A-F001925-L.salsa.univ-amu.fr>
2017-06-02 12:05:58 +0200

    Validation du dernier test de l'exercice 2

--- src/main/java/fr/univ_amu/iut/exercice2/PropertyExampleContinued.java
+++ src/main/java/fr/univ_amu/iut/exercice2/PropertyExampleContinued.java
@@ -28,6 +28,27 @@ public class PropertyExampleContinued {
     }
 
     void bindAndUnbindOnePropertyToAnother() {
-        throw new RuntimeException("Not yet implemented !");
+        {
+            System.out.println();
+            IntegerProperty otherProperty = new SimpleIntegerProperty(0);
+            System.out.println("otherProperty.get() = " + otherProperty.get());
+
+            System.out.println("Binding otherProperty to anIntProperty.");
+            otherProperty.bind(anIntProperty);
+            System.out.println ("otherProperty.get() = " + otherProperty.get());
+
+            System.out.println("Calling anIntProperty.set(7168).");
+            anIntProperty.set(7168);
+            System.out.println ("otherProperty.get() = " + otherProperty.get());
+
+            System.out.println("Unbinding otherProperty from anIntProperty.");
+            otherProperty.unbind();
+            System.out.println ("otherProperty.get() = " + otherProperty.get());
+
+            System.out.println("Calling anIntProperty.set(8192).");
+            anIntProperty.set(8192);
+            System.out.println ("otherProperty.get() = " + otherProperty.get());
+        }
     }
 }
+
--- src/test/java/fr/univ_amu/iut/exercice2/PropertyExampleContinuedTest.java
+++ src/test/java/fr/univ_amu/iut/exercice2/PropertyExampleContinuedTest.java
@@ -20,7 +20,7 @@ public class PropertyExampleContinuedTest {
         System.setOut(out);
     }
 
-    @Ignore
+
     @Test
     public void bindAndUnbindOnePropertyToAnother() throws Exception {
         propertyExample.bindAndUnbindOnePropertyToAnother();
